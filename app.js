6ac81fd01155b5f3c0cfa5b9a01783856be3a027
Alex Silva <alexsaraujo92@gmail.com>
2021-10-05 17:25:03 -0300

    adiciona Ejs e pastas Front

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ const documentDal = require('./DAL/documents')
 const studentDal = require('./DAL/students')
 const app = express()
 
+//configurando a ejs
+app.set('view engine','ejs')
+
+//definindo o caminho da views wjs
+app.set('views', './app/views')
+
+//config de arquivos estáticos
+app.use(express.static('./app'))
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get('/', async(req, res) => {
+    res.render("home/index")
+})
 
 app.get('/contact', async(req, res) => {
     let ret = await contactDal.load(1)
@@ -16,24 +31,17 @@ app.get('/document', async(req, res) => {
     res.send(ret)
 })
 
+/*
 app.get('/', async(req, res) => {
     let ret = await studentDal.load(1)
     res.send(ret)
 })
-
+*/
 
 app.get('/create-contact', async(req, res) => {
     let ret = await contactDal.create(1, 3, 'instagram', 'alexsilva7')
     res.send(ret)
 })
-
-/*
-app.get('/create-document', async(req, res) => {
-    let ret = await documentDal.create()
-    res.send(ret)
-})
-*/
-
 
 app.listen(3000, () => {
     console.log('Escutando na porta 3000')
