1b73bba0959b1e70f46bfff781d8f590ac28093e
Alex Silva <alexsaraujo92@gmail.com>
2021-10-05 17:28:11 -0300

    -db

--- DAL/dbConnection.js
+++ DAL/dbConnection.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg')
 
 const client = new Pool({
-    connectionString: 'postgres://cqbkabwpprswdb:fb314cf470e454f9e381b89d0c03337501e020d52f1111c14d2c78d409c64d8f@ec2-44-199-26-122.compute-1.amazonaws.com:5432/dfgihv8qr5ppl6',
+    connectionString: '',
     ssl: {
         rejectUnauthorized: false,
     }
